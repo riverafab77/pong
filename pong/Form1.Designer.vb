f9f463ff42ca5073a74646338606fd5a3f20b13a
fabrivera <fabrivera99>
2015-04-17 22:43:08 -0600

    First commit, all source code for version 1.1
    Includes manual in Spanish

---
+++ pong/Form1.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pnl_Player1 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.pnl_Player2 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.pnl_Player1.SuspendLayout()
+        Me.pnl_Player2.SuspendLayout()
+        Me.Panel5.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnl_Player1
+        '
+        Me.pnl_Player1.BackColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(150, Byte), Integer), CType(CType(240, Byte), Integer))
+        Me.pnl_Player1.Controls.Add(Me.Panel3)
+        Me.pnl_Player1.Location = New System.Drawing.Point(12, 188)
+        Me.pnl_Player1.Name = "pnl_Player1"
+        Me.pnl_Player1.Size = New System.Drawing.Size(60, 230)
+        Me.pnl_Player1.TabIndex = 0
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.FromArgb(CType(CType(13, Byte), Integer), CType(CType(71, Byte), Integer), CType(CType(161, Byte), Integer))
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel3.Location = New System.Drawing.Point(0, 160)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(60, 70)
+        Me.Panel3.TabIndex = 0
+        '
+        'pnl_Player2
+        '
+        Me.pnl_Player2.BackColor = System.Drawing.Color.FromArgb(CType(CType(139, Byte), Integer), CType(CType(195, Byte), Integer), CType(CType(74, Byte), Integer))
+        Me.pnl_Player2.Controls.Add(Me.Panel4)
+        Me.pnl_Player2.Location = New System.Drawing.Point(133, 120)
+        Me.pnl_Player2.Name = "pnl_Player2"
+        Me.pnl_Player2.Size = New System.Drawing.Size(60, 230)
+        Me.pnl_Player2.TabIndex = 1
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(105, Byte), Integer), CType(CType(30, Byte), Integer))
+        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel4.Location = New System.Drawing.Point(0, 160)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(60, 70)
+        Me.Panel4.TabIndex = 1
+        '
+        'Panel5
+        '
+        Me.Panel5.Controls.Add(Me.Label2)
+        Me.Panel5.Controls.Add(Me.pnl_Player2)
+        Me.Panel5.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel5.Location = New System.Drawing.Point(1015, 0)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(205, 589)
+        Me.Panel5.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Light", 72.0!)
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(3, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(104, 128)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "0"
+        '
+        'Panel6
+        '
+        Me.Panel6.BackColor = System.Drawing.Color.White
+        Me.Panel6.Location = New System.Drawing.Point(252, 458)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(50, 50)
+        Me.Panel6.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Light", 72.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(87, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 128)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "0"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI Light", 72.0!)
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(376, 310)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(751, 128)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Modo ""Hardcore"""
+        Me.Label3.Visible = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI Light", 36.0!)
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(220, 43)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(551, 65)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Golpes para 'Hardcore': 10"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(66, Byte), Integer), CType(CType(66, Byte), Integer), CType(CType(66, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1220, 589)
+        Me.Controls.Add(Me.Panel6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.pnl_Player1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.pnl_Player1.ResumeLayout(False)
+        Me.pnl_Player2.ResumeLayout(False)
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel5.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pnl_Player1 As System.Windows.Forms.Panel
+    Friend WithEvents pnl_Player2 As System.Windows.Forms.Panel
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents Panel4 As System.Windows.Forms.Panel
+    Friend WithEvents Panel5 As System.Windows.Forms.Panel
+    Friend WithEvents Panel6 As System.Windows.Forms.Panel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ pong/Form1.vb
@@ -0,0 +1,255 @@
+﻿Imports System.Windows.Input
+Public Class Form1
+    Dim direction = "TopLeft"
+    Dim player1
+    Dim player2
+
+    Dim x = 3
+    Dim y = 6
+
+    Sub movementTopLeft()
+        Panel6.Top -= x
+        Panel6.Left -= y
+
+        direction = "TopLeft"
+    End Sub
+    Sub movementTopRight()
+        Panel6.Top -= x
+        Panel6.Left += y
+
+        direction = "TopRight"
+    End Sub
+    Sub movementBottomLeft()
+        Panel6.Top += x
+        Panel6.Left -= y
+
+        direction = "BottomLeft"
+    End Sub
+    Sub movementBottomRight()
+        Panel6.Top += x
+        Panel6.Left += y
+
+        direction = "BottomRight"
+    End Sub
+
+    Protected Overrides Function ProcessCmdKey(ByRef msg As Message, keyData As Keys) As Boolean
+        Select Case keyData
+
+            'Jugador 2
+            Case Keys.Up
+                If pnl_Player2.Top > 0 Then
+                    pnl_Player2.Top -= My.Computer.Screen.Bounds.Height / 24
+                End If
+            Case Keys.Down
+                If pnl_Player2.Top < Me.Height - pnl_Player2.Height Then
+                    pnl_Player2.Top += My.Computer.Screen.Bounds.Height / 24
+                End If
+        End Select
+
+
+        Return MyBase.ProcessCmdKey(msg, keyData)
+    End Function
+
+    Dim keysPressed As New HashSet(Of Keys)
+
+    Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+
+        'Add the pressed key into the list
+        keysPressed.Add(e.KeyCode)
+
+        If keysPressed.Contains(Keys.W) AndAlso keysPressed.Contains(Keys.Up) Then
+
+            If pnl_Player1.Top > 0 Then
+                pnl_Player1.Top -= My.Computer.Screen.Bounds.Height / 24
+            End If
+
+            If pnl_Player2.Top > 0 Then
+                pnl_Player2.Top -= My.Computer.Screen.Bounds.Height / 24
+            End If
+
+        End If
+
+        If keysPressed.Contains(Keys.W) AndAlso keysPressed.Contains(Keys.Down) Then
+            If pnl_Player1.Top > 0 Then
+                pnl_Player1.Top -= My.Computer.Screen.Bounds.Height / 24
+            End If
+
+            If pnl_Player2.Top < Me.Height - pnl_Player2.Height Then
+                pnl_Player2.Top += My.Computer.Screen.Bounds.Height / 24
+            End If
+
+        End If
+
+        If keysPressed.Contains(Keys.S) AndAlso keysPressed.Contains(Keys.Down) Then
+            If pnl_Player1.Top < Me.Height - pnl_Player1.Height Then
+                pnl_Player1.Top += My.Computer.Screen.Bounds.Height / 24
+            End If
+
+            If pnl_Player2.Top < Me.Height - pnl_Player2.Height Then
+                pnl_Player2.Top += My.Computer.Screen.Bounds.Height / 24
+            End If
+        End If
+
+
+        If keysPressed.Contains(Keys.S) AndAlso keysPressed.Contains(Keys.Up) Then
+            If pnl_Player1.Top < Me.Height - pnl_Player1.Height Then
+                pnl_Player1.Top += My.Computer.Screen.Bounds.Height / 24
+            End If
+
+            If pnl_Player2.Top > 0 Then
+                pnl_Player2.Top -= My.Computer.Screen.Bounds.Height / 24
+            End If
+        End If
+
+
+        Select Case e.KeyCode
+            Case Keys.W
+                If pnl_Player1.Top > 0 Then
+                    pnl_Player1.Top -= My.Computer.Screen.Bounds.Height / 24
+                End If
+            Case Keys.S
+                If pnl_Player1.Top < Me.Height - pnl_Player1.Height Then
+                    pnl_Player1.Top += My.Computer.Screen.Bounds.Height / 24
+                End If
+
+            Case Keys.Enter
+                Timer1.Interval = 1
+
+            Case Keys.Space
+                Timer1.Start()
+        End Select
+    End Sub
+
+    Private Sub Form1_KeyUp(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyUp
+        'Remove the pressed key from the list 
+        keysPressed.Remove(e.KeyCode)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        'deteccion de obstaculos, muro superior e inferior
+        Select Case Panel6.Top
+            Case Is < 0
+                Select Case direction
+                    Case "TopLeft"
+                        direction = "BottomLeft"
+                    Case "TopRight"
+                        direction = "BottomRight"
+                End Select
+
+            Case Is > Me.Height - Panel6.Height
+                Select Case direction
+                    Case "BottomLeft"
+                        direction = "TopLeft"
+                    Case "BottomRight"
+                        direction = "TopRight"
+                End Select
+        End Select
+
+        'deteccion de obstaculos, paletas
+        Select Case Panel6.Left
+
+
+            Case Is < pnl_Player1.Left + pnl_Player1.Width
+                If Panel6.Top > (pnl_Player1.Top - (Panel6.Height / 2)) And Panel6.Top < pnl_Player1.Top + pnl_Player1.Height Then
+                    If Timer1.Interval > 1 Then
+                        Timer1.Interval -= 1
+                        Label4.Text = "Golpes para 'Hardcore': " & Timer1.Interval
+
+                    Else
+                        Label4.Visible = False
+                        Label3.Visible = True
+                        x += 1
+                        y += 1
+                    End If
+
+                    Select Case direction
+                        Case "TopLeft"
+                            direction = "TopRight"
+
+                        Case "BottomLeft"
+                            direction = "BottomRight"
+                    End Select
+
+                Else
+                    player2 += 1
+                    Label2.Text = player2
+                    Panel6.Top = (Me.Height / 2) - (Panel6.Width / 2)
+                    Panel6.Left = (Me.Width / 2) - (Panel6.Width / 2)
+
+                    If player2 > 2 Then
+                        Label3.Visible = True
+                        Label3.Text = "Jugador 2 a ganado"
+
+                        x = 3
+                        y = 6
+                        Timer1.Interval = 10
+                    End If
+
+                    Timer1.Stop()
+                End If
+
+            Case Is > Me.Width - (pnl_Player2.Right - pnl_Player2.Width)
+
+                If Panel6.Top > (pnl_Player2.Top - (Panel6.Height / 2)) And Panel6.Top < pnl_Player2.Top + pnl_Player2.Height Then
+                    If Timer1.Interval > 1 Then
+                        Timer1.Interval -= 1
+                        Label4.Text = "Golpes para 'Hardcore': " & Timer1.Interval
+
+                    Else
+                        Label4.Visible = False
+                        Label3.Visible = True
+                        x += 1
+                        y += 1
+                    End If
+
+                    Select Case direction
+                        Case "TopRight"
+                            direction = "TopLeft"
+
+                        Case "BottomRight"
+                            direction = "BottomLeft"
+                    End Select
+
+                Else
+                    player1 += 1
+                    Label1.Text = player1
+                    Panel6.Top = (Me.Height / 2) - (Panel6.Width / 2)
+                    Panel6.Left = (Me.Width / 2) - (Panel6.Width / 2)
+
+                    If player1 > 2 Then
+                        Label3.Visible = True
+                        Label3.Text = "Jugador 1 a ganado"
+
+                        x = 3
+                        y = 6
+                        Timer1.Interval = 10
+                    End If
+
+                    Timer1.Stop()
+                End If
+
+        End Select
+
+        If direction = "TopLeft" Then
+            movementTopLeft()
+        End If
+        If direction = "TopRight" Then
+            movementTopRight()
+        End If
+        If direction = "BottomLeft" Then
+            movementBottomLeft()
+        End If
+        If direction = "BottomRight" Then
+            movementBottomRight()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Panel6.Top = (Me.Height / 2) - (Panel6.Width / 2)
+        Panel6.Left = (Me.Width / 2) - (Panel6.Width / 2)
+
+        Label3.Top = (Me.Height / 2) - (Label3.Width / 2)
+        Label3.Left = (Me.Width / 2) - (Label3.Width / 2)
+    End Sub
+End Class
